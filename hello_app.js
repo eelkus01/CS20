2dc2fd571726ec9062166d20003fc3e77d258f84
eelkus01 <130937420+eelkus01@users.noreply.github.com>
2024-04-16 13:13:53 -0400

    Update hello_app.js

--- hello_app.js
+++ hello_app.js
@@ -1,11 +1,15 @@
 // This is a demo for using heroku
 var http = require('http');
+var url = require('url');
 var port = process.env.PORT || 3000;
 // var port = 8080;  //uncomment to run locally
 console.log("This goes to the console window");
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("<h2>Hello World</h2>");
-  res.write ("Success!  This app is deployed online");
+  urlObj = url.parse(req.url, true);
+  id = urlObj.query.id;
+  // res.write("<h2>Hello World</h2>");
+  // res.write ("Success!  This app is deployed online");
+  res.write("The id is: " + id);
   res.end();
 }).listen(port);
